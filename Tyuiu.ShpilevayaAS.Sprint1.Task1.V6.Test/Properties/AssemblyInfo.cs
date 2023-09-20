4cd82857b5baabb9019ecc081afcb8c0567fb117
Tyuiu.ShpilevayaAS@outlook.com <rina.ress@mail.ru>
2023-09-20 23:39:35 +0500

    Task1

---
+++ Tyuiu.ShpilevayaAS.Sprint1.Task1.V6.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShpilevayaAS.Sprint1.Task1.V6.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShpilevayaAS.Sprint1.Task1.V6.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("33f61743-6237-499b-bdd5-d78fcc0ae43b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
