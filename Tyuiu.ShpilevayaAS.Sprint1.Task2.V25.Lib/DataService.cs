c20aa037dd382f0833173f501037e271993511d6
Tyuiu.ShpilevayaAS@outlook.com <rina.ress@mail.ru>
2023-09-22 19:48:41 +0500

    Task2

---
+++ Tyuiu.ShpilevayaAS.Sprint1.Task2.V25.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.ShpilevayaAS.Sprint1.Task2.V25.Lib
+{
+    public class DataService : ISprint1Task2V25
+    {
+        public double ConvertRadsToDegrees(int value)
+        {
+            return value * 180 / 3.14;
+        }
+    }
+}
---
+++ Tyuiu.ShpilevayaAS.Sprint1.Task2.V25.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ShpilevayaAS.Sprint1.Task2.V25.Lib;
+
+namespace Tyuiu.ShpilevayaAS.Sprint1.Task2.V25.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int value = 314;
+            double res = ds.ConvertRadsToDegrees(value);
+            Assert.AreEqual(18000, res);
+        }
+    }
+}
